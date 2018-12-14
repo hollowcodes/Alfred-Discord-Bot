1f9840540e027cbc38a5aebff8b89b54b957f71b
Theo P <teddypeifer@gmail.com>
2018-12-14 14:51:53 +0100

    Update PRIVATE.java

--- test_bot/src/main/java/util/PRIVATE.java
+++ test_bot/src/main/java/util/PRIVATE.java
@@ -2,7 +2,7 @@ package util;
 
 public class PRIVATE {
 
-    public static final String TOKEN = "NTExOTMyMDc0MDk4NDI1ODg2.DsyK7g.Q7svSTcMFY7P-XVUD44kw5wHfZk";
-    public static final String ID = "511932074098425886";
+    public static final String TOKEN = "(bot token here)";
+    public static final String ID = "(bot id here)";
 }
 
